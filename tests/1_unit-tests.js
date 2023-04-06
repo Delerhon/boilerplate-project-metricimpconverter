1041203cc51f788f90740518d3c28033a69d59fc
Delerhon <66553893+Delerhon@users.noreply.github.com>
2023-04-06 18:09:45 +0200

    decimal number test ready

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -4,14 +4,21 @@ const ConvertHandler = require('../controllers/convertHandler.js');
 
 let convertHandler = new ConvertHandler();
 
-const testInput =       '1kg'             // <<<<<<<<<<<<<=====================================
+const wholeNumber   =   '1kg'
+const decimalNumber =   "1,1kg"
 
 suite('Unit Tests', () => {
     // #1
     test('whole number', () => {
-        const input = convertHandler.getNum(testInput)
-        assert.isNumber(input, `Handler gives a number`)
-        assert.equal(input % 1, 0, `Handler can handle whole numbers`);
+        const input = convertHandler.getNum(wholeNumber)
+        assert.isNumber(input,      `Handler gives a number`)
+        assert.equal(input % 1, 0,  `Handler can handle whole numbers`);
+    })
+
+    test('decimal number', () => {
+        const input = convertHandler.getNum(decimalNumber)
+        assert.isNumber(input,      `Handler gives a number`)
+        assert.notEqual(input % 1, 0,  `Handler can handle decimal numbers`);
     })
 
 });
