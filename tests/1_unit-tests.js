2f0171a00690bffcd14c6eab9cf2faaa8eb3a78a
Delerhon <66553893+Delerhon@users.noreply.github.com>
2023-04-06 17:58:57 +0200

    whole number test ready

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -4,6 +4,14 @@ const ConvertHandler = require('../controllers/convertHandler.js');
 
 let convertHandler = new ConvertHandler();
 
-suite('Unit Tests', function(){
+const testInput =       '1kg'             // <<<<<<<<<<<<<=====================================
+
+suite('Unit Tests', () => {
+    // #1
+    test('whole number', () => {
+        const input = convertHandler.getNum(testInput)
+        assert.isNumber(input, `Handler gives a number`)
+        assert.equal(input % 1, 0, `Handler can handle whole numbers`);
+    })
 
 });
