9bf6f608098c6f6595cd5e02aa6a075aa0a4de84
Delerhon <66553893+Delerhon@users.noreply.github.com>
2023-04-07 17:25:32 +0200

    finished the errors and route

--- routes/api.js
+++ routes/api.js
@@ -15,27 +15,45 @@ app.route('/api/convert').get((req, res, next) => {
 
 
 module.exports = (app) => {
-  const strBadNum   = {string: 'Bad input! You didn\'t give a number.'}
-  const strNoInput  = {string: 'No input! Come on and type something in!'}
-  const strNoUnit   = {string: 'No input! Your Unit is bad or isn\'t supported.'}
-  const strBadUnit  = {string: 'Bad input! Your Unit is bad or isn\'t supported.'}
+  const strBadNum           = {string: 'Bad input! You didn\'t give a number.'}
+  const strNoInput          = {string: 'No input! Come on and type something in!'}
+  const strNoUnit           = {string: 'No input! Your Unit is bad or isn\'t supported.'}
+  const strBadUnit          = {string: 'Bad input! Your Unit is bad or isn\'t supported.'}
+  const strDoubleFraction   = {string: 'Bad input! Can\'t handle double fraction.'}
+  const strAllBad           = {string: 'Bad input! Number and unit are invalid.'}
+
+  
+
 
   const convertHandler = new ConvertHandler();
 
   app.route('/api/convert').get((req, res, next) => {
     if (!req.query.input)                     {return res.json(strNoInput)}
     req.query.input   = convertHandler.trimInput(req.query.input)
-    req.query.num     = convertHandler.getNum(req.query.input)
-    if (!req.query.num)                       { return res.json(strBadNum)} 
-    req.query.unit    = convertHandler.getUnit(req.query.input)
-    if (req.query.unit == 'noLettersFound')   { return res.json(strNoUnit)}
-    if (!req.query.unit)                      { return res.json(strBadUnit)}
+    const initNum     = convertHandler.getNum(req.query.input)
+    const initUnit    = convertHandler.getUnit(req.query.input)
     
-      
-    res.json({string: `${initNum} ${initUnitString} converts to ${returnNum} ${returnUnitString}`})
+    // Error responses
+    if ((!initNum | initNum == 'error') &&
+        (initUnit == 'error1' | initUnit == 'error2')){
+          return res.json(strAllBad)
+        }
+    if (!initNum)                       { return res.json(strBadNum) }
+    if (initNum == 'error')             { return res.json(strDoubleFraction) }           
+    if (initUnit == 'error1')           { return res.json(strNoUnit) }
+    if (initUnit == 'error2')           { return res.json(strBadUnit)} 
+
+    // build response values
+    const returnUnit  = convertHandler.getReturnUnit(initUnit)
+    const returnNum   = convertHandler.convert(initNum, initUnit)
+    const string      = convertHandler.getString(initNum,initUnit, returnNum, returnUnit)
+
+    res.json({
+      initNum ,
+      initUnit ,
+      returnNum ,
+      returnUnit ,
+      string
+    })
   })
 };
- /*
- Your return will consist of the 
- initNum, initUnit, returnNum, returnUnit, string spelling out units in the format '{initNum} {initUnitString} converts to {returnNum} {returnUnitString}' with the result rounded to 5 decimals.
- */
